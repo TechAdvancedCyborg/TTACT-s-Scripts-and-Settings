104da65013e11e725326910f2d5824386390b39a
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-07-27 19:14:54 +0200

    Update update.sh

--- Scripts/Linux/GithubAutoUpdatingCode/update.sh
+++ Scripts/Linux/GithubAutoUpdatingCode/update.sh
@@ -1,8 +1,14 @@
 [ "$(whoami)" != "root" ] && exec sudo sh -- "$0" "$@"
 
 git clone https://github.com/vinceliuice/Mojave-gtk-theme
+git clone https://github.com/vinceliuice/WhiteSur-kde
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
+
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme
 git clone https://github.com/vinceliuice/McMojave-circle
+git clone https://github.com/vinceliuice/WhiteSur-kde
 git clone https://github.com/daniruiz/Flat-Remix
+
 git clone https://gitlab.com/hamadmarri/gamma-text-editor
 git clone https://github.com/bartobri/no-more-secrets
 
@@ -11,9 +17,25 @@ git pull
 ./install.sh
 cd ..
 
+cd WhiteSur-kde
+git pull
+./install.sh
+cd ..
+
+cd WhiteSur-gtk-theme
+git pull
+./install.sh
+cd ..
+
+
 cd McMojave-circle
 git pull
 ./install.sh -a
+cd ..
+
+cd WhiteSur-icon-theme
+git pull
+./install.sh
 cd ..
 
 cd Flat-Remix
@@ -26,7 +48,6 @@ git pull
 chmod +x ./setup.sh
 ./setup.sh
 cd ..
-
 
 cd no-more-secrets
 git pull
